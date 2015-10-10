7f4de2c33d515ae771fef019d08dc0773f9f421b
rakirox <palmeroh@gmail.com>
2015-10-10 16:33:31 -0500

    tasks publishers

--- server/publishers/tasks.js
+++ server/publishers/tasks.js
@@ -1,3 +1,7 @@
+Meteor.publish("tasks", function (projectId, state, userId) {
+    return Tasks.find({projectId : projectId,state:state, userId:userId});
+});
+
 Meteor.publish("tasks", function (projectId, state) {
-    return Tasks.find({});
+    return Tasks.find({projectId : projectId,state:state});
 });
