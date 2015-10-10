ba3342f721700907483cb6f4d2a1510768c9e8d0
jesus lerma sanchez <shuyojl@gmail.com>
2015-10-10 17:20:52 -0500

    start pomo timer wip

---
+++ server/methods/tasks.js
@@ -0,0 +1,17 @@
+Meteor.methods({
+	'insertTask': function (task) {
+		var currentUserId = Meteor.userId();
+		tasks = Task.insert({
+			name: task.name,
+			description: task.description,
+			subTasks: task.subTasks,
+			projectId: task.projectId,
+			ownerId: currentUserId,
+			userId: currentUserId
+		});
+	},
+	'deleteTask': function (taskId) {
+		var currentUserId = Meteor.userId();
+		task.remove({ _id: taskId, ownerId: currentUserId});
+	}
+});
